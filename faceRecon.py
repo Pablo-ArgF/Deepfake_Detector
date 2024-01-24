227a693812b6a5cfd128ef0c1e3c4906a511140b
Pablo-ArgF <98907723+Pablo-ArgF@users.noreply.github.com>
2024-01-24 12:47:33 +0100

    Train full dataset 6% accuracy :(

--- faceRecon.py
+++ faceRecon.py
@@ -1,7 +1,9 @@
 import cv2
 import os
-import numpy as np
+
 from sklearn.base import BaseEstimator, TransformerMixin
+import concurrent.futures
+import pandas as pd
 
 class FaceExtractor(BaseEstimator, TransformerMixin):
     def __init__(self, n=10, output_dir=None):
@@ -32,9 +34,9 @@ class FaceExtractor(BaseEstimator, TransformerMixin):
                         detected_faces = self.face_cascade.detectMultiScale(gray, 1.3, 5)
                         for (x, y, w, h) in detected_faces:
                             face_img = cv2.resize(frame[y:y+h, x:x+w], (64, 64))  # Redimensiona la imagen a 64x64
-                            # Añade la imagen y la etiqueta a los arrays
+                            # Añade la imagen y la etiqueta a los arrays convirtiendolos a float64 e int respectivamente
                             faces.append(face_img)
-                            labels.append(label)
+                            labels.append(int(label))
                             if self.output_dir:
                                 videoName = video_path.split('\\')[-1].split('/')[-1].split('.')[0]
                                 #crea una carpeta para el video en cuestión
@@ -45,4 +47,67 @@ class FaceExtractor(BaseEstimator, TransformerMixin):
                 else:
                     break
             cap.release()
-        return np.array(faces), np.array(labels)
+        df = pd.DataFrame({'face': faces, 'label': labels})
+        return df
+    
+
+
+
+class FaceExtractorMultithread(BaseEstimator, TransformerMixin):
+    def __init__(self, n=10, output_dir=None, max_workers=None):
+        self.n = n
+        self.output_dir = output_dir
+        self.max_workers = max_workers
+        if self.output_dir and not os.path.exists(self.output_dir):
+            os.makedirs(self.output_dir)
+
+    def fit(self, X, y=None):
+        return self
+
+    def process_video(self, row):
+        face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+        #row es una tupla de [index, row], no nos interesa el index
+        row = row[1]
+        faces = []
+        labels = []
+        img_count = 0
+        video_path = row['video']
+        label = row['label']
+        cap = cv2.VideoCapture(video_path)
+        frame_count = 0
+        while cap.isOpened():
+            ret, frame = cap.read()
+            if ret:
+                frame_count += 1
+                if frame_count % self.n == 0:
+                    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+                    detected_faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+                    for (x, y, w, h) in detected_faces:
+                        face_img = cv2.resize(frame[y:y+h, x:x+w], (64, 64))  # Redimensiona la imagen a 64x64
+                        # Añade la imagen y la etiqueta a los arrays
+                        faces.append(face_img)
+                        labels.append(label)
+                        if self.output_dir:
+                            videoName = video_path.split('\\')[-1].split('/')[-1].split('.')[0]
+                            #crea una carpeta para el video en cuestión
+                            if not os.path.exists(os.path.join(self.output_dir, videoName)):
+                                os.makedirs(os.path.join(self.output_dir, videoName))
+                            img_count += 1
+                            cv2.imwrite(os.path.join(os.path.join(self.output_dir,videoName), f'{videoName}_face_{img_count}.jpg'), face_img)
+            else:
+                break
+        cap.release()
+        return faces, labels
+
+    def transform(self, X, y=None):
+        faces = []
+        labels = []
+        with concurrent.futures.ThreadPoolExecutor(max_workers=self.max_workers) as executor:
+            # Start the futures and store them in a dictionary
+            futures = {executor.submit(self.process_video, row): row for _, row in X.iterrows()}
+            for future in concurrent.futures.as_completed(futures):
+                result_faces, result_labels = future.result()
+                faces.extend(result_faces)
+                labels.extend(result_labels)
+        df = pd.DataFrame({'face': faces, 'label': labels})
+        return df
--- model.py
+++ model.py
@@ -16,29 +16,11 @@ from tensorflow.keras import regularizers
 from keras.optimizers import SGD
 from keras.utils import plot_model
 from keras.models import load_model
-from faceRecon import FaceExtractor
+from faceRecon import FaceExtractorMultithread, FaceExtractor
 
-
-#cargamos del archivo List_of_testing_videos.txt los nombres de los videos de test y su categoria
-# guardado en el archivo con estructura <0/1 ruta_video>
-# 0 -> fake
-# 1 -> real
-def testingDataset(baseDir):
-    testing_videos = []
-    testing_labels = []
-    with open('Datasets\CelebDB\Celeb-DF-v2\List_of_testing_videos.txt', 'r') as file:
-        for line in file:
-            testing_videos.append(os.path.join(baseDir,line.split()[1]))
-            testing_labels.append(line.split()[0])
-    dataFrame = pd.DataFrame({'video': testing_videos, 'label': testing_labels})
-    return dataFrame
-
-#cargamos los videos el dataset de entrenamiento, para ello iternamos por la carpeta de videos y guardamos en 
-# el datasframe el nombre y categoría de aquellos que no han aparecido en el dataset de test
-def trainingDataset(baseDir):
-    training_videos = []
-    training_labels = []
-    testingVideos =  testingDataset(baseDir)['video'].values.tolist()
+def loadData(baseDir):
+    videos = []
+    labels = []
     # Iterate over the folders of videos inside Celeb-DF-v2
     for folder in os.listdir(baseDir):
         folder_path = os.path.join(baseDir, folder)
@@ -46,32 +28,35 @@ def trainingDataset(baseDir):
             continue
         for video in os.listdir(folder_path):
             video_path = os.path.join(folder_path, video)
-            if video not in testingVideos:
-                training_videos.append(video_path)
-                if (folder.split('-')[1] == 'real'):
-                    training_labels.append(1)
-                else:
-                    training_labels.append(0)
-    dataFrame = pd.DataFrame({'video': training_videos, 'label': training_labels})
+            videos.append(video_path)
+            if (folder.split('-')[1] == 'real'):
+                labels.append(1)
+            else:
+                labels.append(0)
+    dataFrame = pd.DataFrame({'video': videos, 'label': labels})
+
+    # Reduce el tamaño del dataset para que sea más fácil de manejar
+    #dataFrame = dataFrame.sample(1000, random_state=42)
+
+    face_extractor = FaceExtractor(n=100)#, max_workers=5)
+    print('Extracting faces from videos...')
+    dataFrame= face_extractor.transform(dataFrame)
     return dataFrame
 
 
-df_train = trainingDataset(baseDir='Datasets\CelebDB\Celeb-DF-v2')
-df_test = testingDataset(baseDir='Datasets\CelebDB\Celeb-DF-v2')
+df = loadData(baseDir='Datasets\CelebDB\Celeb-DF-v2')
+
+print('Dividing dataset into train and test...')
+# Dividir el dataset en train y test
+X = df.drop(['label'], axis = 1)
+y = df['label']
 
 
-
-# Reducir el tamaño de los dataframes a 200 filas para entrenamiento y 100 filas para test, de forma reproducible
-df_train = df_train.sample(10, random_state=42)
-df_test = df_test.sample(5, random_state=42)
-
+X_train, X_test, y_train, y_test = train_test_split(X,y, test_size=0.2, random_state=42, stratify = y)
+X_train = np.stack(X_train['face'], axis=0)
+X_test = np.stack(X_test['face'], axis=0)
 
 
-
-
-face_extractor = FaceExtractor(n=10, output_dir='imgs')
-X_train, y_train = face_extractor.transform(df_train)
-X_test, y_test = face_extractor.transform(df_test)
 
 y_train = y_train.astype(float)
 y_test = y_test.astype(float)
@@ -79,11 +64,11 @@ y_test = y_test.astype(float)
 
 model = Sequential()
 model.add(Input(shape=(64, 64, 3)))
-model.add(Conv2D(32, (3, 3), activation='relu'))
+model.add(Conv2D(10, (3, 3), activation='relu'))
 model.add(MaxPooling2D((2, 2)))
-model.add(Conv2D(64, (3, 3), activation='relu'))
-model.add(MaxPooling2D((2, 2)))
-model.add(Conv2D(64, (3, 3), activation='relu'))
+model.add(Conv2D(20, (3, 3), activation='relu'))
+#model.add(MaxPooling2D((2, 2)))
+#model.add(Conv2D(10, (3, 3), activation='relu'))
 model.add(Flatten())
 model.add(Dense(64, activation='relu'))
 model.add(Dense(1, activation='softmax'))
@@ -92,12 +77,17 @@ model.compile(optimizer='adam',
               loss='binary_crossentropy',
               metrics=['accuracy'])
 
+print('Started training...')
+
 model.fit(X_train, y_train, epochs=10, validation_data=(X_test, y_test))
 
 #evaluamos el modelo
 loss, acc = model.evaluate(X_test, y_test, verbose=0)
 print('Test Accuracy: %.3f' % acc)
 print('Test Loss: %.3f' % loss)
+
+#exportamos el modelo
+model.save('model.h5')
 
 
 
