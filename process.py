575470e68fc4b6ced4bacd71cbff180b52b1aa59
Pablo-ArgF <pablo.af.important@gmail.com>
2024-02-10 21:38:07 +0100

    Updated process module

--- process.py
+++ process.py
@@ -1,42 +1,58 @@
 import os
-import numpy as np
 import pandas as pd
-from sklearn.metrics import confusion_matrix, classification_report
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler
-from tensorflow.keras import Input
-from tensorflow.keras.layers import Dense, MaxPooling2D, Conv2D, Flatten
-from tensorflow.keras.models import Sequential
-from keras.models import load_model
 from faceRecon import FaceExtractorMultithread, FaceExtractor
 
 
-baseDir='Datasets\CelebDB\Celeb-DF-v2'
+baseDir='E:\TFG\Datasets\FaceForensics'
+nameDataset = 'FaceForensics'
+destinationDir='E:\TFG\Datasets\dataframes'
+numFragments = 10
+eachNframes = 20
+
 videos = []
 labels = []
-# Iterate over the folders of videos inside Celeb-DF-v2
-for folder in os.listdir(baseDir):
-    folder_path = os.path.join(baseDir, folder)
-    if not os.path.isdir(folder_path):
-        continue
-    for video in os.listdir(folder_path):
-        video_path = os.path.join(folder_path, video)
-        videos.append(video_path)
-        if (folder.split('-')[1] == 'real'):
-            labels.append(1)
+currentIsReal = True
+
+def processFolder(path):
+    #check if current folder indicates real/fake
+    sections = path.split('-')
+    if(sections[-1] == 'real' or sections[-1] == 'fake'):
+        currentIsReal = sections[-1] == 'real'
+
+    for item in os.listdir(path):
+        currentPath = os.path.join(path,item)
+        
+        if not os.path.isdir(currentPath):
+            processVideo(currentPath)
         else:
-            labels.append(0)
+            processFolder(currentPath)
+
+def processVideo(path):
+    videos.append(path)
+    if(currentIsReal):
+        labels.append(1) 
+    else:
+        labels.append(0)
+    
+
+dataFrame = processFolder(baseDir)
 dataFrame = pd.DataFrame({'video': videos, 'label': labels})
+print(len(dataFrame))
+dataFrame = dataFrame.sample(10, random_state=42)
+
+
+
+        
+
 
 # Reduce el tamaño del dataset para que sea más fácil de manejar
-#dataFrame = dataFrame.sample(1200, random_state=42)
+dataFrame = dataFrame.sample(10, random_state=42)
 
-face_extractor = FaceExtractorMultithread(n=20)
+face_extractor = FaceExtractorMultithread(n=eachNframes)
 print('Extracting faces from videos...')
-# Dividimos el dataframe en 6 partes para procesarlo mejor en ram
-fragmentSize = int(len(dataFrame)/6)
-for i in range(6):
-    print(f'Processing fragment {i+1}/6')
+fragmentSize = int(len(dataFrame)/numFragments)
+for i in range(numFragments):
+    print(f'Processing fragment {i+1}/{numFragments}')
     processed = face_extractor.transform(dataFrame.iloc[fragmentSize*i : fragmentSize*(i+1)])
     # Guardamos el fragmento procesado en un fichero hdf
-    processed.to_hdf(f'.\dataframes\CelebDB\dataframe{i}_{len(dataFrame)}videos.h5', key=f'df{i}', mode='w')
+    processed.to_hdf(f'{destinationDir}\dataframe{i}_{nameDataset}.h5', key=f'df{i}', mode='w')
