3abe2c2aa5e6effbce8a6b223b6b781087051f8e
Pablo-ArgF <pablo.af.important@gmail.com>
2024-02-22 17:51:00 +0100

    Update processor to class

--- process.py
+++ process.py
@@ -1,55 +1,72 @@
 import os
 import pandas as pd
-from faceRecon import FaceExtractorMultithread, FaceExtractor
+from faceRecon import FaceExtractorMultithread,FaceExtractorMultithread_DeepLearning, FaceExtractor
 
 
-baseDir='E:\TFG\Datasets\FaceForensics'
-nameDataset = 'FaceForensics'
-destinationDir='E:\TFG\Datasets\dataframes'
-numFragments = 40
-eachNframes = 20
 
-videos = []
-labels = []
-currentIsReal = True
+class VideoProcessor():
+    def __init__(self,baseDir,nameDataset,destinationDir,numFragments,eachNfragments):
+        self.baseDir=baseDir
+        self.nameDataset = nameDataset
+        self.destinationDir= destinationDir
+        self.numFragments = numFragments
+        self.eachNframes = eachNfragments
 
-def processFolder(path):
-    #check if current folder indicates real/fake
-    sections = path.split('-')
-    if(sections[-1] == 'real' or sections[-1] == 'fake'):
-        currentIsReal = sections[-1] == 'real'
+        self.videos = []
+        self.labels = []
+        self.currentIsReal = True
 
-    for item in os.listdir(path):
-        currentPath = os.path.join(path,item)
-        if not os.path.isdir(currentPath):
-            processVideo(currentPath)
+    def processFolder(self, path = None):
+        if(path == None):
+            path = self.baseDir
+        #check if current folder indicates real/fake
+        sections = path.split('-')
+        if(sections[-1] == 'real' or sections[-1] == 'fake'):
+            if sections[-1] == 'real':
+                self.currentIsReal = True
+            else:
+                self.currentIsReal = False
+
+
+        for item in os.listdir(path):
+            currentPath = os.path.join(path,item)
+            if not os.path.isdir(currentPath):
+                self.processVideo(currentPath)
+            else:
+                self.processFolder(currentPath)
+
+    def processVideo(self,path):
+        #check it is an pm4 file, if not return
+        fragments = path.split('.')
+        if(fragments[-1] != 'mp4'):
+            return
+        self.videos.append(path)
+        if(self.currentIsReal):
+            self.labels.append(1) 
         else:
-            processFolder(currentPath)
+            self.labels.append(0)
 
-def processVideo(path):
-    #check it is an pm4 file, if not return
-    fragments = path.split('.')
-    if(fragments[-1] != 'mp4'):
-        return
-    videos.append(path)
-    if(currentIsReal):
-        labels.append(1) 
-    else:
-        labels.append(0)
+    def getDataFrame(self):
+        self.processFolder()
+        return pd.DataFrame({'video': self.videos, 'label': self.labels})
     
+    def processFaces(self):
+        dataFrame = self.getDataFrame()
 
-dataFrame = processFolder(baseDir)
-dataFrame = pd.DataFrame({'video': videos, 'label': labels})
-print(len(dataFrame))
 
-# Reduce el tamaño del dataset para que sea más fácil de manejar
-#dataFrame = dataFrame.sample(10, random_state=42)
+        #dataFrame = dataFrame.sample(10, random_state=42)
+        print(f'Processing {len(dataFrame)} videos')
+        
+        face_extractor = FaceExtractorMultithread(n=self.eachNframes)
+        print('Extracting faces from videos...')
+        fragmentSize = int(len(dataFrame)/self.numFragments)
+        for i in range(63, self.numFragments):
+            print(f'Processing fragment {i+1}/{self.numFragments}')
+            processed = face_extractor.transform(dataFrame.iloc[fragmentSize*i : fragmentSize*(i+1)])
+            # Guardamos el fragmento procesado en un fichero hdf
+            processed.to_hdf(f'{self.destinationDir}\dataframe{i}_{self.nameDataset}.h5', key=f'df{i}', mode='w')
 
-face_extractor = FaceExtractorMultithread(n=eachNframes)
-print('Extracting faces from videos...')
-fragmentSize = int(len(dataFrame)/numFragments)
-for i in range(numFragments):
-    print(f'Processing fragment {i+1}/{numFragments}')
-    processed = face_extractor.transform(dataFrame.iloc[fragmentSize*i : fragmentSize*(i+1)])
-    # Guardamos el fragmento procesado en un fichero hdf
-    processed.to_hdf(f'{destinationDir}\dataframe{i}_{nameDataset}.h5', key=f'df{i}', mode='w')
+    
+processor = VideoProcessor('E:\TFG\Datasets\FaceForensics','FaceForensics','E:\TFG\Datasets\dataframes\FaceForensics',100,50 )
+
+processor.processFaces()
