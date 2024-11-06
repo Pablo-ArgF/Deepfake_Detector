c4189c42bf67ee4642612e73404432f920175782
Pablo-ArgF <pablo.af.important@gmail.com>
2024-11-06 23:11:28 +0100

    [dockerize] Conexion con back y avances en predicción

--- backend/app.py
+++ backend/app.py
@@ -1,10 +1,14 @@
+from datetime import datetime
+import time
 import os
-from flask import Flask, request, jsonify
+from flask import Flask, jsonify, request, url_for
+from apscheduler.schedulers.background import BackgroundScheduler #Scheduler to delete the added user images 
 from tensorflow.keras.models import load_model
 from flask_cors import CORS
 import numpy as np
 from PIL import Image
 import base64
+import shutil
 from logging.handlers import RotatingFileHandler
 from werkzeug.utils import secure_filename
 import sys
@@ -32,11 +36,13 @@ CORS(app, resources={r"/*": {"origins": "*"}})
 # Increase maximum content length to 4Gb
 app.config['MAX_CONTENT_LENGTH'] = 4 * 1024 * 1024 *1024  # 4Gb
 app.config['VIDEO_UPLOAD_FOLDER'] = '/tmp'
-app.config['UPLOAD_FOLDER'] = '/home/pabloarga/Deepfake_Detector/frontend/build/results'
+app.config['UPLOAD_FOLDER'] = '/home/pabloarga/Deepfake_Detector/frontend/build/results' #TODO remove
 app.config['UPLOAD_FOLDER_REF'] = './results' #REference for the frontend src
 app.config['SELECTED_CNN_MODEL'] = '2024-06-20 08.29.00' 
 app.config['SELECTED_RNN_MODEL'] = '2024-06-26 16.22.50'
 app.config['RNN_MODEL_SEQUENCE_LENGTH'] = 20
+app.config['STATIC_IMAGE_FOLDER'] = '/path/to/static/frames'
+EXPIRY_TIME = 60 #3 * 60 * 60  # 3 hours in seconds
 
 # Load the cnn model
 path = f"/app/models/{app.config['SELECTED_CNN_MODEL']}/model{app.config['SELECTED_CNN_MODEL']}.keras"  
@@ -45,6 +51,34 @@ model = load_model(path, safe_mode=False, compile=False)
 # Load the rnn model
 pathSequences = f"/app/models/{app.config['SELECTED_RNN_MODEL']}/model{app.config['SELECTED_RNN_MODEL']}.keras"  
 modelSequences = load_model(pathSequences, safe_mode=False, compile=False)
+
+#---------------------------------------------------
+# Job to delete the images on the server after EXPIRY_TIME time
+def cleanup_old_files(video_name):
+    """Cleanup function to remove video and frame files older than 3 hours."""
+    video_folder = os.path.join(app.config['STATIC_IMAGE_FOLDER'], video_name)
+    
+    if os.path.exists(video_folder):
+        shutil.rmtree(video_folder)
+        app.logger.info(f"Deleted images for video: {video_name}")
+    
+    video_file_path = os.path.join(app.config['VIDEO_UPLOAD_FOLDER'], video_name)
+    if os.path.exists(video_file_path):
+        os.remove(video_file_path)
+        app.logger.info(f"Deleted video file: {video_name}")
+
+def schedule_cleanup(video_name, uploadTime):
+    """Schedule cleanup for each video after 3 hours."""
+    delay = EXPIRY_TIME - (time.time() - uploadTime)
+    if delay > 0:
+        run_date = datetime.fromtimestamp(time.time() + delay)
+        scheduler.add_job(func=cleanup_old_files, args=[video_name], trigger='date', run_date=run_date)
+        app.logger.info(f"Scheduled cleanup for video {video_name} in {delay/3600:.2f} hours.")
+
+# Initialize APScheduler
+scheduler = BackgroundScheduler()
+scheduler.start()
+#---------------------------------------------------
  
 
 faceExtractor = FaceExtractorMultithread() 
@@ -55,14 +89,31 @@ def image_to_base64(image_path):
 
 def save_images(frames, video_name):
     """
-    Guarda las imágenes en el directorio de carga.
+    Save frames to a static folder and return a list of their URLs.
     """
-    image_files = []
+    urls = []
+    base_folder = os.path.join(app.config['STATIC_IMAGE_FOLDER'], video_name)
+    os.makedirs(base_folder, exist_ok=True)
+    
     for i, frame in enumerate(frames):
-        file_path = os.path.join(app.config['UPLOAD_FOLDER'], f'{video_name}_frame_{i}.png')
-        cv2.imwrite(file_path, frame)
-        image_files.append(os.path.join(app.config['UPLOAD_FOLDER_REF'], f'{video_name}_frame_{i}.png'))
-    return image_files
+        # Ensure the frame is a numpy array
+        if isinstance(frame, np.ndarray):
+            # Convert the numpy array to a PIL image
+            image = Image.fromarray(frame)
+        
+            # Define the filename and path
+            filename = f"{video_name}_frame_{i}.jpg"
+            file_path = os.path.join(base_folder, filename)
+            
+            # Save the image
+            image.save(file_path)  # Saves the image as a .jpg file
+
+            # Construct URL using url_for to serve it from the static folder
+            urls.append(url_for('static', filename=f'frames/{video_name}/{filename}', _external=True))
+        else:
+            app.logger.warning(f"Frame {i} is not a valid numpy array.")
+    
+    return urls
 
 def save_sequences(sequences, video_name):
     """
@@ -151,9 +202,12 @@ def predict():
     minVal = np.min(predictions)
     range_ = maxVal - minVal
 
-    # Save the images and get their paths
-    video_frame_files = save_images(videoFrames, video_name)
-    processed_frame_files = save_images(processedFrames, f'{video_name}_processed')
+    # Save images and get URLs
+    video_frame_urls = save_images(videoFrames, video_name)
+    processed_frame_urls = save_images(processedFrames, f'{video_name}_processed')
+
+    # Schedule cleanup after 3 hours
+    schedule_cleanup(video_name, uploadTime = time.time())
 
     return jsonify({
         'predictions': {
@@ -166,8 +220,8 @@ def predict():
         'min':minVal,
         'range':range_,
         'nFrames': len(predictions),
-        'videoFrames': video_frame_files,
-        'processedFrames': processed_frame_files
+        'videoFrames': video_frame_urls,
+        'processedFrames': processed_frame_urls
     }), 200
 
 @app.route('/api/predict/sequences', methods=['POST'])
@@ -201,24 +255,26 @@ def predictSequences():
         for i in range(app.config['RNN_MODEL_SEQUENCE_LENGTH']):
             resultPredictions.append(float(value[0]))  
         
-    # Save the images and get their paths
-    video_frame_files = save_sequences(videoFrames, video_name)
-    processed_frame_files = save_sequences(processedSequences, f'{video_name}_processed')
+    # Save images and get URLs
+    video_frame_urls = save_images(videoFrames, video_name)
+    processed_frame_urls = save_images(processedSequences, f'{video_name}_processed')
+
+    # Schedule cleanup after 3 hours
+    schedule_cleanup(video_name, upload_time = time.time())
 
     return jsonify({
         'predictions': {
             'id': video_name,
-            'data': [{'x': index, 'y': value} for index, value in enumerate(resultPredictions)]
+            'data': [{'x': index, 'y': value} for index, value in enumerate(predictions)]
         },
-        'mean': float(mean),
-        'var':float(var),
-        'max':float(maxVal),
-        'min':float(minVal),
-        'range':float(range_),
-        'nSequences': len(processedSequences),
-        'sequenceSize': app.config['RNN_MODEL_SEQUENCE_LENGTH'],
-        'videoFrames': video_frame_files,
-        'processedFrames': processed_frame_files
+        'mean': mean,
+        'var':var,
+        'max':maxVal,
+        'min':minVal,
+        'range':range_,
+        'nFrames': len(predictions),
+        'videoFrames': video_frame_urls,
+        'processedFrames': processed_frame_urls
     }), 200
 
 
